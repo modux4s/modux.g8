8493fc302cdd261bddb29b987ba71c588aac3e0c
Joaquín Velázquez Camacho <joaquinvelazquezcamacho@gmail.com>
2020-08-25 20:17:48 -0300

    template fix

--- src/main/g8/src/main/scala/$package$/$module$.scala
+++ src/main/g8/src/main/scala/$package$/$module$.scala
@@ -3,7 +3,7 @@ package $package$
 import modux.core.api.{ModuleX, Service}
 import modux.model.context.Context
 
-case class ModuleRegister(context: Context) extends ModuleX {
+case class $module;format="Camel"$(context: Context) extends ModuleX {
   override def providers: Seq[Service] = Seq(
     
   )
