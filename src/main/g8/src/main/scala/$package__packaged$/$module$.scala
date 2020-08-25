8c814953527905343ae2474a7c6a961f9b937306
Joaquín Velázquez Camacho <joaquinvelazquezcamacho@gmail.com>
2020-08-25 20:51:25 -0300

    some fixes

--- src/main/g8/src/main/scala/$package__packaged$/$module$.scala
+++ src/main/g8/src/main/scala/$package__packaged$/$module$.scala
@@ -2,10 +2,11 @@ package $package$
 
 import modux.core.api.{ModuleX, Service}
 import modux.model.context.Context
+import $package$.services.$service$
 
 case class $module;format="Camel"$(context: Context) extends ModuleX {
   override def providers: Seq[Service] = Seq(
-    
+    $service;format="Camel"$(context)
   )
 }
 
